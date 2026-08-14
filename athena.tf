2ac3beabe82b38935cf079c845e0d0e184eeaaf1
msaeed721 <msaeed@aofund.org>
2026-08-14 12:56:16 -0500

    Add Athena workgroup with query cost controls

---
+++ athena.tf
@@ -0,0 +1,25 @@
+resource "aws_athena_workgroup" "demo" {
+  name = "aws-de-demo"
+
+  configuration {
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = true
+
+    # 100 MB maximum scan per query
+    bytes_scanned_cutoff_per_query = 104857600
+
+    result_configuration {
+      output_location = "s3://${aws_s3_bucket.data_lake.bucket}/athena-results/"
+
+      encryption_configuration {
+        encryption_option = "SSE_S3"
+      }
+    }
+  }
+
+  tags = {
+    Project     = "AWS Data Engineering Demo"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
